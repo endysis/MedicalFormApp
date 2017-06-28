57d49fc233de3e133f3977383787e861de6164e5
O'Shea Douglas <shea_o@me.com>
2017-06-28 23:58:00 +0100

    addedVRMEd

---
+++ WPFJA/WPFJA/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WPFJA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PatientInfo patient = new PatientInfo();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            resetAll();
+        }
+        
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            gatherFormData();
+            sendToDB();
+        }
+
+        public void resetAll()
+        {
+            NameText.Text = String.Empty;
+            AgeText.Text = String.Empty;
+            StatusBox.SelectedIndex = 0;
+            string checkBoxName = "CB";
+            for (int i = 0; i <= 7; i++)
+            {
+                checkBoxName = checkBoxName + i.ToString();
+                var checkBox = (CheckBox)this.FindName(checkBoxName);
+                checkBox.IsChecked = false;
+                checkBoxName = "CB";
+            }
+        }
+
+        public void gatherFormData() {
+            patient.FullName = NameText.Text.ToString();
+            patient.Age = AgeText.Text.ToString();
+            patient.Status = StatusBox.Text;
+            patient.Symptons = formCheckBoxString();
+        }
+
+        public void sendToDB() {
+            string URI = "http://localhost/sendPatient.php";
+            string parameters = "p_name=" + patient.FullName + "&p_age=" + patient.Age + "&p_status=" + patient.Status + "&p_symptons=" + patient.Symptons;
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string HtmlResult = wc.UploadString(URI, parameters);
+                MessageBox.Show("Form Sent");
+                resetAll();
+            }
+        }
+
+        public string formCheckBoxString() {
+            string checkBoxName = "CB";
+            string output = "";
+            for (int i = 0; i <= 7; i++)
+            {
+                checkBoxName = checkBoxName + i.ToString();
+                var checkBox = (CheckBox)this.FindName(checkBoxName);
+
+                if (checkBox.IsChecked == true) {
+                    output = output + checkBox.Content.ToString() + ",";
+                }
+                checkBoxName = "CB";
+            }
+            return output;
+        }
+    }
+}
---
+++ WPFJA/WPFJA/PatientInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFJA
+{
+    class PatientInfo
+    {
+        string pFullName;
+        string pAge;
+        string pStatus;
+        string pSymptons;
+
+        public string FullName
+        {
+            get { return pFullName;}
+            set { pFullName = value;}
+        }
+
+        public string Age
+        {
+            get {return pAge;}
+            set { pAge = value;}
+        }
+
+        public string Status
+        {
+            get {return pStatus;}
+            set {pStatus = value;}
+        }
+        public string Symptons
+        {
+            get { return pSymptons; }
+            set { pSymptons = value; }
+        }
+
+
+
+
+
+        /*
+        public string ReturnFullName() {
+         return pFullName;
+        }
+        */
+
+
+    }
+}
